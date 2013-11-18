3a2ffd5bfe351876b5229f65655299a25a6d6c9d
Pau Pérez <saulopefa@gmail.com>
2013-11-18 10:26:49 +0100

    Test coordinates validity and use fixtures

--- test/models/sensor_test.rb
+++ test/models/sensor_test.rb
@@ -2,22 +2,27 @@ require 'test_helper'
 
 class SensorTest < ActiveSupport::TestCase
   test "should not save sensor without location" do
-    sensor = Sensor.new(type: "solar", active: true)
-    assert !sensor.save
+    sensor = sensors(:no_location)# Sensor.new(type: "solar", active: true)
+    assert !sensor.save, "Sensor saved without location"
   end
 
+  test "should not save non-float coordinates as location" do
+    sensor = Sensor.new(latitude: "a", longitude: "b") # The fixture casts them to float
+    assert !sensor.save, "Sensor saved with non-float coordinates location"
+  end 
+
   test "should not save sensor without type" do
-    sensor = Sensor.new(type: "some", latitude: 1.2, longitude: 1.2)
+    sensor = sensors(:no_type)
     assert !sensor.save, "Sensor saved without type"
   end
 
   test "its type should be either solar or wind" do
-    sensor = Sensor.new(type: "another", latitude: 1.2, longitude: 1.2)
+    sensor = sensors(:unknown_type)
     assert !sensor.save, "Sensor saved with an unknown type"
   end
 
   test "should set active true as default" do
-    sensor = Sensor.new(type: "wind", latitude:1.2, longitude: 1.2)
+    sensor = sensors(:no_active_set)
     assert_equal true, sensor.active
   end
 end
